6340a77ec5bda4ca92e33c69c37c57527a16cd3f
hachi8833 <hachi8833@gmail.com>
2026-02-18 16:18:57 +0900

    Implement DialogComponent

---
+++ app/components/dialog_component.rb
@@ -0,0 +1,36 @@
+class DialogComponent < ViewComponent::Base
+  renders_one :title
+  renders_one :description
+  renders_one :body
+  renders_one :footer
+
+  def initialize(
+    id:,
+    confirm_text: "確認",
+    cancel_text: "キャンセル",
+    confirm_form: nil,
+    closedby: "any",
+    show_close_button: true,
+    **options
+  )
+    @id = id
+    @confirm_text = confirm_text
+    @cancel_text = cancel_text
+    @confirm_form = confirm_form
+    @closedby = closedby
+    @show_close_button = show_close_button
+    @options = options
+  end
+
+  private
+
+  attr_reader :id, :confirm_text, :cancel_text, :confirm_form, :closedby, :show_close_button, :options
+
+  def title_id
+    "#{id}-title"
+  end
+
+  def description_id
+    "#{id}-desc"
+  end
+end
